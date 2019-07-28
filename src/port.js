3ee630880b2159ffccd43ef2e38548533e7d73ff
Holly Fanthorpe <hollyfanthorpe@gmail.com>
2019-07-28 16:01:19 +0100

    Added immediately invoked function to port.js

--- src/port.js
+++ src/port.js
@@ -1,18 +1,24 @@
-function Port(name) {
-  this.name = name;
-  this.ships = [];
-}
-
-Port.prototype = {
-
-  addShip: function (ship) {
-    this.ships.push(ship);
-  },
-
-  removeShip: function (ship) {
-    const indexOfShip = this.ships.indexOf(ship)
-    this.ships.splice(indexOfShip,1)
+(function exportPort() {
+  function Port(name) {
+    this.name = name;
+    this.ships = [];
   }
-};
 
-module.exports = Port;
+  Port.prototype = {
+
+    addShip: function (ship) {
+      this.ships.push(ship);
+    },
+
+    removeShip: function (ship) {
+      const indexOfShip = this.ships.indexOf(ship)
+      this.ships.splice(indexOfShip,1)
+    },
+  };
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = Port;
+  } else {
+    window.Port = Port;
+  }
+}());
