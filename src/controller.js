42e822df83f2abbf6a1779d59a4725c1113c8ebd
Holly Fanthorpe <hollyfanthorpe@gmail.com>
2019-07-28 16:05:03 +0100

    Added immediately invoked function to controller.js

--- src/controller.js
+++ src/controller.js
@@ -1,20 +1,28 @@
 /* eslint-disable func-names */
-function Controller() {
-  this.initialiseSea();
-}
+(function exportController() {
+  function Controller() {
+    this.initialiseSea();
+  }
 
-Controller.prototype = {
-  initialiseSea: function () {
-    const backgrounds = [
-      './images/water0.png',
-      '/images/water1.png',
-    ];
+  Controller.prototype = {
+    initialiseSea: function () {
+      const backgrounds = [
+        './images/water0.png',
+        '/images/water1.png',
+      ];
 
-    let backgroundCounter = 0;
+      let backgroundCounter = 0;
 
-    window.setInterval(() => {
-      document.querySelector('#viewport').style.backgroundImage = `url('${backgrounds[backgroundCounter % backgrounds.length]}')`;
-      backgroundCounter += 1;
-    }, 1000);
-  },
-};
+      window.setInterval(() => {
+        document.querySelector('#viewport').style.backgroundImage = `url('${backgrounds[backgroundCounter % backgrounds.length]}')`;
+        backgroundCounter += 1;
+      }, 1000);
+    },
+  };
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = Controller;
+  } else {
+    window.Controller = Controller;
+  }
+}());
